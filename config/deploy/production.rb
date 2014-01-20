7c5e61f768a0647a840d3d248aa529da6cb7318d
metacortex <9ruvie@gmail.com>
2014-01-21 00:49:20 +0900

    TR-182 #comment db:create, nginx enabled-sites 정리

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '198.199.107.89', user: 'deployer', roles: %w{web app db}#, my_property: :my_value
+server '162.243.231.45', user: 'deployer', roles: %w{web app db}#, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
